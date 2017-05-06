9418468047affb11ba9016a8049ad6d2f4261db7
Alex Epstein <epsteina@wit.edu>
2017-05-06 02:57:08 -0400

    Starting to run different functions depending on args

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo mkdir /usr/local/insomnia-lite
-sudo cp assets /usr/local/insomnia-lite
+sudo cp sleep.png /usr/local/bin
+sudo cp sleep.ico /usr/local/bin
 sudo cp insomnia-lite /usr/local/bin
 sudo chmod a+x /usr/local/bin/insomnia-lite
