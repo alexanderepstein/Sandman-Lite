eebdb3d2de46b2847e2808de8519c8dd3bf1d730
Alex Epstein <epsteina@wit.edu>
2017-05-06 23:25:32 -0400

    Added help, doing more sleep time math

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-sudo cp sleep.png /usr/local/bin
-sudo cp sleep.ico /usr/local/bin
+# Just copies the files over from the project directory into usr/local/bin
+# This automatically adds insomnia-lite to the path on both OSX and Linux
+# Version 1.0.0
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    sudo cp sleep.ico /usr/local/bin
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    sudo cp sleep.png /usr/local/bin
+fi
 sudo cp insomnia-lite /usr/local/bin
 sudo chmod a+x /usr/local/bin/insomnia-lite
