bcee4fd43d2e3a765cbd33a020b7b87cbb1c30d7
Alex Epstein <epsteina@wit.edu>
2017-05-08 02:31:37 -0400

    Got linux noti working

--- noti.sh
+++ noti.sh
@@ -2,11 +2,12 @@
 #export DISPLAY=/private/tmp/com.apple.launchd.5jte4rZAmH/org.macosforge.xquartz:0
 #export SHLVL=1
 #export SECURITYSESSIONID=186a5
-env > ~/git/env.log
-echo 'Start of log session' 2> ~/git/noti.log
-  if [[ "$OSTYPE" == "linux-gnu" ]]; then
-      notify-send "Insomnia" "Time to take a rest"  -u critical -t 7000 -i sleep.ico 2> ~/git/linux.log
 
-  elif [[ "$OSTYPE" == "darwin"* ]]; then
+echo 'Start of log session' 2> ~/git/noti.log
+
+  if (uname -o | grep Linux);then
+      export DISPLAY=:0
+      notify-send "Insomnia" "Time to take a rest"  -u critical -t 7000 -i sleep.ico 2> ~/git/linux.log
+  elif [ "$OSTYPE" == "darwin"* ]; then
       terminal-notifier -title "Insomnia" -message "Time to take a rest"  -sound "default"  -group rest -timeout 7 -appIcon sleep.png 2> ~/git/darwin.log
   fi
