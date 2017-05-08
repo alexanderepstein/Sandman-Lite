6d5b7e31707b5ccc39393136a01949c6cf43bad1
Alex Epstein <epsteina@wit.edu>
2017-05-08 05:46:05 -0400

    Got rid of bug on linux
    On linux if you set a wakeup time and one of the generated sleep times was before the current time it would show that notification as soon as you set the time. TLDR Only one balloon at a time on linux

--- assets/noti.sh
+++ assets/noti.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
   if (uname -o | grep Linux);then
-      export DISPLAY=:0
-      notify-send "Insomnia" "Time to take a rest"  -u critical -t 7000 -i sleep.ico 2> ~/git/linux.log
+      notify-send "Insomnia" "Time to take a rest"  -u critical -t 7000 -i sleep.ico 
   elif [[ "$OSTYPE" == "darwin"* ]]; then
       reattach-to-user-namespace insomnia-lite -t
   fi
