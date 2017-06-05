9208c0e703f40e992eddc5cb5dd953c897d13d6b
Alex Epstein <epsteina@wit.edu>
2017-06-04 20:40:50 -0400

    Fixing error check

--- assets/supdate.sh
+++ assets/supdate.sh
@@ -10,9 +10,9 @@ grep() {
 }
 
 latestVersion=$(curl -s https://api.github.com/repos/alexanderepstein/Sandman-Lite/tags | grep -Po '"name":.*?[^\\]",'| head -1 | cut -c10-15)
-cd  ~ || { echo 'Update Failed'; exit 1; }
+cd  ~ || echo 'Update Failed' && exit 1
 sudo rm -r Sandman-Lite  2> /dev/null
 git clone https://github.com/alexanderepstein/Sandman-Lite || (echo "Couldnt download latest version" ; exit 1)
-cd Sandman-Lite || { echo 'Update Failed'; exit 1; }
+cd Sandman-Lite ||  echo 'Update Failed' && exit 1 
 git checkout $latestVersion 2> /dev/null
 ./install.sh
