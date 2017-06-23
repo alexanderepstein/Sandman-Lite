10c5618a976ca363a0dc33ed6998c5d42418772a
Alex Epstein <epsteina@wit.edu>
2017-06-23 17:45:21 -0400

    Cleaning up install & uninstall script

--- install.sh
+++ install.sh
@@ -10,73 +10,41 @@ echo "sandman-lite requires your password to continue installation"
 sudo cp assets/sleep.png /usr/local/sbin ||  { echo "Installation failed" ; exit 1 ;}
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    sudo apt-get install at notify-osd libnotify-bin -y ||  { echo "Installation failed" ; exit 1 ;}
+  sudo apt-get install at notify-osd libnotify-bin -y ||  { echo "Installation failed" ; exit 1 ;}
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 
 
   command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew Now"; \
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
 
 
-if [[ $(brew ls --versions terminal-notifier )> /dev/null ]]; then
-echo
-echo "terminal-notifier already installed :)"
-echo
-echo
-echo
-else
-  echo
-  echo "Installing terminal-notifier"
-  echo
-  brew install terminal-notifier ||  { echo "Installation failed" ; exit 1 ;}
-  echo
-  echo
-fi
+  if [[ $(brew ls --versions terminal-notifier )> /dev/null ]]; then
+    echo "terminal-notifier already installed :)"
+  else
+    echo "Installing terminal-notifier"
+    brew install terminal-notifier ||  { echo "Installation failed" ; exit 1 ;}
+  fi
 
-if [[ $(brew ls --versions reattach-to-user-namespace )> /dev/null ]]; then
-echo
-echo "reattach-to-user-namespace already installed :)"
-echo
-echo
-echo
-else
-  echo
-  echo "Installing reattach-to-user-namespace"
-  echo
-  brew install reattach-to-user-namespace ||  { echo "Installation failed" ; exit 1 ;}
-  echo
-  echo
-fi
+  if [[ $(brew ls --versions reattach-to-user-namespace )> /dev/null ]]; then
+    echo "reattach-to-user-namespace already installed :)"
+  else
+    echo "Installing reattach-to-user-namespace"
+    brew install reattach-to-user-namespace ||  { echo "Installation failed" ; exit 1 ;}
+  fi
 
-if [[ $(brew ls --versions grep )> /dev/null ]]; then
-echo
-echo "ggrep already installed :)"
-echo
-echo
-echo
-else
-  echo
-  echo "Installing ggrep"
-  echo
-  brew install grep > /dev/null  ||  { echo "Installation failed" ; exit 1 ;}
-  echo
-  echo
-fi
+  if [[ $(brew ls --versions grep )> /dev/null ]]; then
+    echo "ggrep already installed :)"
+  else
+    echo "Installing ggrep"
+    brew install grep > /dev/null  ||  { echo "Installation failed" ; exit 1 ;}
+  fi
 
-if [[ $(brew ls --versions coreutils )> /dev/null ]]; then
-echo
-echo "coreutils already installed :)"
-echo
-echo
-echo
-else
-  echo
-  echo "Installing coreutils"
-  echo
-  brew install coreutils ||  { echo "Installation failed" ; exit 1 ;}
-  echo
-  echo
-fi
+  if [[ $(brew ls --versions coreutils )> /dev/null ]]; then
+    echo "coreutils already installed :)"
+  else
+    echo "Installing coreutils"
+    brew install coreutils ||  { echo "Installation failed" ; exit 1 ;}
+  fi
 fi
 sudo cp sandman-lite /usr/local/bin ||  { echo "Installation failed" ; exit 1 ;}
 sudo chmod a+x /usr/local/bin/sandman-lite ||  { echo "Installation failed" ; exit 1 ;}
@@ -85,14 +53,11 @@ sudo chmod a+x /usr/local/sbin/noti.sh ||  { echo "Installation failed" ; exit 1
 sudo cp assets/supdate.sh /usr/local/sbin ||  { echo "Installation failed" ; exit 1 ;}
 sudo chmod a+x /usr/local/sbin/supdate.sh ||  { echo "Installation failed" ; exit 1 ;}
 sudo chmod a+x uninstall.sh ||  { echo "Installation failed" ; exit 1 ;}
-echo
-echo
 echo -n "( •_•)"
 sleep .75
 echo -n -e "\r( •_•)>⌐■-■"
 sleep .75
 echo -n -e "\r               "
-
 echo  -e "\r(⌐■_■)"
 sleep .5
 echo
--- uninstall.sh
+++ uninstall.sh
@@ -12,8 +12,6 @@ sudo rm /usr/local/sbin/sleep.png  ||  { echo "Uninstall failed" ; exit 1 ;}
 sudo rm /usr/local/bin/sandman-lite ||  { echo "Uninstall failed" ; exit 1 ;}
 sudo rm /usr/local/sbin/noti.sh ||  { echo "Uninstall failed" ; exit 1 ;}
 sudo rm /usr/local/sbin/supdate.sh ||  { echo "Uninstall failed" ; exit 1 ;}
-echo
-echo
 echo  -n "(⌐■_■)"
 sleep .75
 echo -n -e "\r( •_•)>⌐■-■"
@@ -21,7 +19,4 @@ sleep .75
 echo -n -e "\r             "
 echo -n -e "\r( •_•)"
 sleep .5
-echo
-
-
 echo "sandman-lite successfully removed"
